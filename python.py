b85963b0c67264e321f1897cbe732e0be42f629a
Loyola User <Confer40@luc.edu>
2019-02-23 11:51:03 -0600

    adding rps in progress

---
+++ python.py
@@ -0,0 +1,26 @@
+import random
+
+# variables to keep track of # of wins
+user_wins = 0
+computer_wins = 0
+
+while user_ wins < 3 and computer_wins < 3
+
+    # get users choice
+	user_choice = raw_ input("Rock, paper or scissors") 
+	
+	# get computer's random choice
+	computer_ choice == random. randint(1,3)
+	  
+# 1 + rock, 2 = paper, 3 + scissors
+    computer word = ""
+    if computer choice == 1:
+       computer_word + "rock"
+    elif computer choice == 2:
+        computer_word + "paper"
+     elif computer choice == 3:
+        computer_word + "scissors"
+
+    # determine winner!		
+	  if user_choice == "rock" and computer_word == "paper" 
+	     print ("computer wins!")
